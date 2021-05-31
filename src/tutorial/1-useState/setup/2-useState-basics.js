c721142ea7e59ae9af5c1380eadbdf841de12d71
TucanRase <tommy.karlsen@hotmail.es>
2021-05-31 12:06:34 +0100

    init

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+	// console.log("hello world");
+	// const value = useState(1)[0];
+	// const handler = useState(1)[1];
+	const [text, setText] = useState("random Title");
+	const handleClick = () => {
+		if (text === "random title") {
+			setText("hello world");
+		} else {
+			setText("random title");
+		}
+	};
+	return (
+		<React.Fragment>
+			<h1>{text}</h1>
+			<button className='btn' onClick={handleClick}>
+				Change title
+			</button>
+		</React.Fragment>
+	);
 };
 
 export default UseStateBasics;
